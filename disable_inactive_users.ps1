b5c2e14222ee9406a9f2c088c7aafa9d8a5b410d
wajdimanai <126868727+wajdimanai@users.noreply.github.com>
2023-03-03 11:49:34 +0100

    Update disable_inactive_users.ps1

--- disable_inactive_users.ps1
+++ disable_inactive_users.ps1
@@ -1,13 +1,13 @@
 $users = Get-LocalUser
 foreach ($user in $users) {
-    if ($user.Name -like "*built*" -or $user.Name -like "*hobbit*" -or $user.Description -like "*built-in*") {
+    if ($user.Name -like "*built*" -or $user.Name -like "*hobbit*" -or $user.Description -like "*built-in*" -or $user.Description -like "*managed and used by the system*") {
         continue
     }
-    
+
     $lastLogon = $user.LastLogon
     if ($lastLogon) {
         $inactiveDays = (New-TimeSpan -Start $lastLogon -End (Get-Date)).Days
-        if ($inactiveDays -gt 45) {
+        if ($inactiveDays -lt 45) {
             Disable-LocalUser -Name $user.Name
             Set-LocalUser -Name $user.Name -Description "Account disabled due to inactivity"
         }
