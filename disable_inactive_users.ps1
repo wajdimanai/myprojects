b9c6379855d4b56d159829e792958e79be885cd9
wajdimanai <126868727+wajdimanai@users.noreply.github.com>
2023-03-03 11:20:51 +0100

    Update disable_inactive_users.ps1

--- disable_inactive_users.ps1
+++ disable_inactive_users.ps1
@@ -1,9 +1,10 @@
 $users = Get-LocalUser
 foreach ($user in $users) {
-    if ($user.Name -like "*built*" -or $user.Name -like "*hobbit*") {continue} #skip any account that has hobbit or built (add whatever name you want to exclude)
-    
-    if ((Get-Date) - $user.LastLogon -gt 45d) #test if user logon date>45, meaning inactive
-    { 
-        Disable-LocalUser -Name $user.Name
+    while ($user.Name -notlike "*built*" -and $user.Name -notlike "*hobbit*") {
+        if ((Get-Date) - $user.LastLogon -gt 45d) {
+            Disable-LocalUser -Name $user.Name
+            break
+        }
+        break
     }
 }
